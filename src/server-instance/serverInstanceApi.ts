bc4026ff10703e9ac0bddef6c022096910981dcc
Harsh Rohila <rohilaharsh@gmail.com>
2023-08-17 20:55:19 +0530

    changed data source for server instances

--- src/server-instance/serverInstanceApi.ts
+++ src/server-instance/serverInstanceApi.ts
@@ -2,18 +2,32 @@ import axios from "axios"
 import { Observable, defer, map } from "rxjs"
 
 export function getServerInstances(): Observable<ServerInstance[]> {
-  const apiUrl = "https://piped-instances.kavin.rocks/"
+  const apiUrl = "https://raw.githubusercontent.com/wiki/TeamPiped/Piped-Frontend/Instances.md"
 
   return defer(() => axios.get(apiUrl)).pipe(
     map(response => response.data),
-    map(data =>
-      data.map(d => ({
-        name: d.name,
-        apiUrl: d.api_url,
-        locations: d.locations,
-        hasCdn: d.cdn
-      }))
-    )
+    map(data => {
+      const instances = []
+      var skipped = 0
+      const lines = data.split("\n")
+      lines.map(line => {
+        const split = line.split("|")
+        if (split.length >= 4) {
+          if (skipped < 2) {
+            skipped++
+            return
+          }
+          instances.push({
+            name: split[0].trim(),
+            apiurl: split[1].trim(),
+            locations: split[2].trim(),
+            hasCdn: split[3].trim() === "Yes" ? true : false
+          })
+        }
+      })
+
+      return instances
+    })
   )
 }
 
