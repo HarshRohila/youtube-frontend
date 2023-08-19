db5643cff6ed4b5ad634d46e251359bab88958ac
Harsh Rohila <rohilaharsh@gmail.com>
2023-08-19 12:53:30 +0530

    added test for play from time

--- cypress/e2e/video-page.cy.js
+++ cypress/e2e/video-page.cy.js
@@ -2,4 +2,9 @@ describe("template spec", () => {
   it("passes", () => {
     cy.visit("/")
   })
+
+  it("Visiting video page with time, starts video from that time", () => {
+    cy.visit("/#/videos/8JnfIa84TnU?t=72.983353")
+    cy.get("video").should("have.prop", "currentTime", 72.983353)
+  })
 })
