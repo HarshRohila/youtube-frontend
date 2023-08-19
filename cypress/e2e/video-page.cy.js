b6732e60794b91aee37c070721a3bdbf3e399eb6
Harsh Rohila <rohilaharsh@gmail.com>
2023-08-19 13:02:10 +0530

    added failing test

--- cypress/e2e/video-page.cy.js
+++ cypress/e2e/video-page.cy.js
@@ -3,8 +3,15 @@ describe("template spec", () => {
     cy.visit("/")
   })
 
-  it("Visiting video page with time, starts video from that time", () => {
-    cy.visit("/#/videos/8JnfIa84TnU?t=72.983353")
-    cy.get("video").should("have.prop", "currentTime", 72.983353)
+  describe("Play Video from time in Link", () => {
+    it("works if video NOT having SKIPABLE part in start", () => {
+      cy.visit("/#/videos/8JnfIa84TnU?t=72.983353")
+      cy.get("video").should("have.prop", "currentTime", 72.983353)
+    })
+
+    it("works if video having SKIPABLE part in start", () => {
+      cy.visit("/#/videos/XatXy6ZhKZw?t=102.466939")
+      cy.get("video").should("have.prop", "currentTime", 102.466939)
+    })
   })
 })
