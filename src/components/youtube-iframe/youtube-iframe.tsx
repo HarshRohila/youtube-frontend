9d9a4072e23141ecdc0012412ccdd1db10178066
Harsh Rohila <rohilaharsh@gmail.com>
2026-01-19 18:09:27 +0530

    added youtube player

---
+++ src/components/youtube-iframe/youtube-iframe.tsx
@@ -0,0 +1,16 @@
+import { Component, h } from "@stencil/core"
+
+@Component({
+  tag: "youtube-iframe",
+  // styleUrl: "x-notification.scss",
+  shadow: true
+})
+export class YoutubeIframe {
+  // @Prop() videoId: string
+
+  render() {
+    return <iframe id="ytplayer" width="100%" height="100%"
+		src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+		frameborder="0"></iframe>
+  }
+}
